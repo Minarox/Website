41370fcdc64246b6c6396e657349ab0ff97a85eb
Minarox <contact@minarox.fr>
2022-08-24 15:45:17 +0200

    Add fontawesome library

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faYoutube,
+  faTwitch,
+  faTwitter,
+  faDiscord,
+  faInstagram,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
-createApp(App).mount("#app");
+library.add(faYoutube, faTwitch, faTwitter, faDiscord, faInstagram, faGithub);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
