481b4d8404a71207eeb0d2043e368fa65d59b731
Mathis S.M. <m.sema@exatech-group.com>
2025-01-27 12:08:58 +0100

    build: Add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://minarox.fr',
-  integrations: [sitemap()]
+  integrations: [sitemap(), partytown()]
 });
