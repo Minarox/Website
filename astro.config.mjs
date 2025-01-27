fd5f39ee4001da5b711b95c9b7ab44fa6cc8fed9
Mathis S.M. <m.sema@exatech-group.com>
2025-01-27 12:33:55 +0100

    build: Add astro-compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
+import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://minarox.fr',
-  integrations: [sitemap(), partytown()]
+	site: 'https://minarox.fr',
+	integrations: [sitemap(), partytown(), compressor()]
 });
