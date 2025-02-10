cb5336570022faccd017756334cb0ae5759c139c
Mathis S.M. <m.sema@exatech-group.com>
2025-02-10 09:32:21 +0000

    feat: #29 Add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,21 @@
 /// <reference types="vitest" />
 import { getViteConfig } from "astro/config";
+import { configDefaults } from "vitest/config";
 
 export default getViteConfig({
-  test: {
-    include: ["test/**/*.test.ts"]
-  }
+    test: {
+        include: ["test/**/*.test.ts"],
+        exclude: [...configDefaults.exclude],
+        coverage: {
+            reporter: ["text", "json-summary", "json"],
+            include: ["src/**/*"],
+            reportOnFailure: true,
+            thresholds: {
+                lines: 60,
+                branches: 60,
+                functions: 60,
+                statements: 60
+            }
+        }
+    }
 });
